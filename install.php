93d76717490a713a44f4fb62e2cea1cbe0f264e9
tico88612 <tico88612@yahoo.com.tw>
2020-12-13 13:56:13 +0800

    Install Script Rewrite

--- install.php
+++ install.php
@@ -16,38 +16,37 @@ $SQL = "
     ALTER TABLE `idf` ADD PRIMARY KEY (`idfid`);
     ALTER TABLE `idf` MODIFY `idfid` int(11) NOT NULL AUTO_INCREMENT;
 
-    DROP TABLE IF EXISTS `search`;
+    
     CREATE TABLE IF NOT EXISTS `search` (
     `SearchId` int(11) NOT NULL,
-    `SearchString` varchar(100) DEFAULT NULL
+    `SearchString` varchar(100) NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
     ALTER TABLE `search` ADD PRIMARY KEY (`SearchId`);
     ALTER TABLE `search` MODIFY `SearchId` int(11) NOT NULL AUTO_INCREMENT;
 
-    DROP TABLE IF EXISTS `searchresult`;
+    
     CREATE TABLE IF NOT EXISTS `searchresult` (
     `SearchResultId` int(11) NOT NULL,
     `SearchId` varchar(50) NOT NULL,
-    `WhiteListId` int(11) NOT NULL,
-    `SearchResultRate` int(11) NOT NULL,
+    `WhiteListId` int(11) NOT NULL DEFAULT '0',
+    `SearchResultRate` double NOT NULL,
     `Link` varchar(1000) NOT NULL,
-    `Title` varchar(1000) NOT NULL,
-    `Content` longtext NOT NULL
+    `Title` varchar(1000) NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
     ALTER TABLE `searchresult` ADD PRIMARY KEY (`SearchResultId`);
     ALTER TABLE `searchresult` MODIFY `SearchResultId` int(11) NOT NULL AUTO_INCREMENT;
 
-    DROP TABLE IF EXISTS `sentence`;
+    
     CREATE TABLE IF NOT EXISTS `sentence` (
-    `id` int(11) NOT NULL,
+    `sentence_id` int(11) NOT NULL,
     `search_result_id` int(11) NOT NULL,
-    `sentences` varchar(500) NOT NULL,
-    `sentence_grade` int(11) NOT NULL
+    `sentences` longtext NOT NULL,
+    `sentence_grade` double NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-    ALTER TABLE `sentence` ADD PRIMARY KEY (`id`), ADD KEY `search_result_id` (`search_result_id`);
+    ALTER TABLE `sentence` ADD PRIMARY KEY (`sentence_id`), ADD KEY `search_result_id` (`search_result_id`);
+    ALTER TABLE `sentence` MODIFY `sentence_id` int(11) NOT NULL AUTO_INCREMENT;
     ALTER TABLE `sentence` ADD CONSTRAINT `search_result_id` FOREIGN KEY (`search_result_id`) REFERENCES `searchresult` (`SearchResultId`);
 
-    DROP TABLE IF EXISTS `WhiteList`;
     CREATE TABLE IF NOT EXISTS `WhiteList` (
     `WhiteListId` int(10) UNSIGNED NOT NULL,
     `WhiteListName` varchar(50) NOT NULL,
