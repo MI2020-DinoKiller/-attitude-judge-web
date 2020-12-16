59f25f41e8ccaf3db35fe302d9b6f993e4268459
tico88612 <tico88612@yahoo.com.tw>
2020-12-16 14:11:10 +0800

    Remove Unused Code

--- api/create_search.php
+++ api/create_search.php
@@ -8,28 +8,6 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 use PhpAmqpLib\Exception\AMQPRuntimeException;
 
-// if (isset($_GET['q']) && $_GET['q'] != "" && isset($_GET['q']) && $_GET['q'] != "") {
-//     $query = $_GET['q'];
-
-//     $connection = new AMQPStreamConnection($MQServer, $MQPort, $MQUsername, $MQPassword);
-//     $channel = $connection->channel();
-//     $channel->queue_declare($MQQueue, false, true, false, false);
-
-//     $sql = "INSERT INTO `search`(`SearchString`, `email`) VALUES (?, ?)";
-//     $sth = $conn->prepare($sql);
-
-//     $sth->execute(array($query));
-//     $insert_id = $conn->lastInsertId();
-
-//     $data = array('searchText' => $query, 'id' => $insert_id);
-//     $msg = new AMQPMessage(
-//             json_encode($data),
-//             array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
-//         );
-
-//     $channel->basic_publish($msg, '', $MQQueue);
-//     header("Location: /search.php?id=" . $insert_id);
-// }
 
 try {
     if (!isset($_POST['q'])) {
