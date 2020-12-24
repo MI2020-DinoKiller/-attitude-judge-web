ad2dda30a0acb0879105ec43ef2a835195d3270d
tico88612 <tico88612@yahoo.com.tw>
2020-12-24 20:54:38 +0800

    sort score article

--- api/search.php
+++ api/search.php
@@ -7,7 +7,7 @@ if (isset($_GET['id']) && intval($_GET['id']) && isset($_GET['last']) && intval(
     $q_id = intval($q_id);
     $last_id = $_GET['last'];
     $last_id = intval($last_id);
-    $sql = "SELECT SearchResultId, Title, SearchResultRate FROM searchresult WHERE SearchId = ? AND SearchResultId > ?";
+    $sql = "SELECT SearchResultId, Title, SearchResultRate FROM searchresult WHERE SearchId = ? AND SearchResultId > ? ORDER BY SearchResultRate";
     $sth = $conn->prepare($sql);
     $sth->execute(array($q_id, $last_id));
     $result = $sth->fetchAll();
@@ -17,7 +17,7 @@ else if (isset($_GET['id']) && intval($_GET['id']))
 {
     $q_id = $_GET['id'];
     $q_id = intval($q_id);
-    $sql = "SELECT SearchResultId, Title, SearchResultRate FROM searchresult WHERE SearchId = ?";
+    $sql = "SELECT SearchResultId, Title, SearchResultRate FROM searchresult WHERE SearchId = ? ORDER BY SearchResultRate";
     $sth = $conn->prepare($sql);
     $sth->execute(array($q_id));
     $result = $sth->fetchAll();
--- js/search.js
+++ js/search.js
@@ -23,6 +23,54 @@ function queryString ()
     }
     return query_string;
 }
+// 閉包含數
+(function() {
+  /**
+   * Decimal adjustment of a number.
+   *
+   * @param {String}  type  The type of adjustment.
+   * @param {Number}  value The number.
+   * @param {Integer} exp   The exponent (the 10 logarithm of the adjustment base).
+   * @returns {Number} The adjusted value.
+   */
+  function decimalAdjust(type, value, exp) {
+    // If the exp is undefined or zero...
+    if (typeof exp === 'undefined' || +exp === 0) {
+      return Math[type](value);
+    }
+    value = +value;
+    exp = +exp;
+    // If the value is not a number or the exp is not an integer...
+    if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+      return NaN;
+    }
+    // Shift
+    value = value.toString().split('e');
+    value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+    // Shift back
+    value = value.toString().split('e');
+    return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+  }
+
+  // Decimal round
+  if (!Math.round10) {
+    Math.round10 = function(value, exp) {
+      return decimalAdjust('round', value, exp);
+    };
+  }
+  // Decimal floor
+  if (!Math.floor10) {
+    Math.floor10 = function(value, exp) {
+      return decimalAdjust('floor', value, exp);
+    };
+  }
+  // Decimal ceil
+  if (!Math.ceil10) {
+    Math.ceil10 = function(value, exp) {
+      return decimalAdjust('ceil', value, exp);
+    };
+  }
+})();
 let last = 0, interval_num, resultZeroTime = 0;
 function GetTemplate(id, title, score)
 {
@@ -37,7 +85,7 @@ function GetTemplate(id, title, score)
     .append(
         $('<td>')
         .append(
-            score
+            Math.round10(parseFloat(score), -5)
         )
     );
     // <tr onclick="window.open('https://google.com');">
@@ -54,7 +102,7 @@ function IntervalGetSearchResult()
         console.log(result);
         result.forEach(element => {
             if (element["SearchResultRate"] < 0) {
-                $('#negative').prepend(GetTemplate(element["SearchResultId"], element["Title"], element["SearchResultRate"]))
+                $('#negative').append(GetTemplate(element["SearchResultId"], element["Title"], element["SearchResultRate"]))
             }
             else if (element["SearchResultRate"] > 0) {
                 $('#positive').prepend(GetTemplate(element["SearchResultId"], element["Title"], element["SearchResultRate"]))
